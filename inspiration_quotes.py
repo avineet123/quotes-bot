9a47c4871942f390cb0ba2059d610ab514e17d92
Avineet Kumar <avineetkumar4@gmail.com>
2018-12-29 00:58:45 -0800

    quotes-bot

---
+++ inspiration_quotes.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 25 02:31:25 2018
+
+@author: My Pc
+"""
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import pandas as pd
+
+bot=ChatBot('Inspiration Quotes')
+bot.set_trainer(ListTrainer)
+df=pd.read_csv('out_data1.csv',encoding ='latin1')
+new = df["hrefs"].str.split("src=t_", n = 1, expand = True)
+df['quotes_type']=new[1]
+author = df["lines"].str.split(".-", n = 1, expand = True)
+df["quotes_lines"]=author[0]
+dataset=df.drop(['lines', 'hrefs'], axis=1)
+d=dict()
+df_new = dataset.groupby('quotes_type').agg({'quotes_lines': ', '.join}).reset_index()
+final_df=df_new[['quotes_type','quotes_lines']]
+for index, row in final_df.iterrows():
+    ques=row['quotes_type']
+    ans=row['quotes_lines']
+    bot.train([ques, ans])
+
+while True:
+    message=input('You:')
+    if message.strip() !='Bye':
+
+        reply=bot.get_response(final_df[final_df['quotes_type']==str(message)])
+        print('Chatbot :',reply)
+    if message.strip()=='Bye':
+        print('Chatbot : Bye')
+        break
---
+++ quotes/spiders/quotes_job.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from quotes.items import QuotesItem
+from scrapy.http import Request
+from urllib.parse import urljoin
+
+
+class QuotesJobSpider(scrapy.Spider):
+    name = 'quotes_job'
+    allowed_domains = ['brainyquote.com']
+    start_urls = ['https://www.brainyquote.com']
+
+    def parse(self, response):
+        products = response.xpath('//div[contains(@class,"bqLn")]/a/@href').extract()
+        for p in products:
+            url = urljoin(response.url, p)
+            yield scrapy.Request(url, callback=self.parse_product)
+
+    def parse_product(self, response):
+        for info in response.xpath('//div[contains(@id, "quotesList")]').extract():
+            for t in info:
+                hrefs = response.xpath('//div[contains(@class, "qti-listm")]/a/@href').extract()
+                lines = response.xpath("//div[@class='qti-listm']//a/img/@alt").extract()
+
+                for item in zip(hrefs,lines):
+                    new_item = QuotesItem()
+                    new_item['hrefs'] = item[0]
+                    new_item['lines'] = item[1]
+                    yield new_item
+
+
+
+
+
+
+
+
